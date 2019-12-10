70edf1206b5d772ded6fd73d47fcf7bbcf229cb2
Magnus Krona <magnus.krona@gmail.com>
2019-12-10 15:00:44 +0100

    script should not be injected if already exists

--- src/Weavy.WebView.Plugin.Forms/Shared/WebView.Helpers.shared.cs
+++ src/Weavy.WebView.Plugin.Forms/Shared/WebView.Helpers.shared.cs
@@ -17,9 +17,15 @@ try{{
             type: 'POST'
         }}).then(function(response){{
             location.reload();
-            Native('signInCompleteCallback', true);
+            Native('signInCompleteCallback', 'ok!');
+        }}).fail(function(e){{
+            Native('signInCompleteCallback', 'error 1: ' + e.message);
         }});  
-}} catch(e){{}}
+}} catch(e){{
+
+Native('signInCompleteCallback', 'error 2: ' + e);
+
+}}
 ";
 
         public static string ScriptChecker = @"
@@ -55,156 +61,132 @@ try{
 ";
 
         public static string Scripts = @"
-var weavyAppScripts = weavyAppScripts || {};
+if(typeof weavyAppScripts === 'undefined') {
 
-/***************************************/
-/* Debug script for test purposes only */
-/***************************************/
-weavyAppScripts.debug = (function(){    
-    function test(){
-        alert('debug!');  
-    }
-    /*test();*/
-})();
+    var weavyAppScripts = weavyAppScripts || {};
 
-
-
-
-
-
-/********************************************/
-/* Register user for azure notification hub */
-/********************************************/
-weavyAppScripts.push = (function(){    
-    function register(){
-        var userId = $('body').data('user');        
-        var userGuid = $('body').data('guid');
+    /********************************************/
+    /* Register user for azure notification hub */
+    /********************************************/
+    weavyAppScripts.push = (function(){    
+        function register(){
+            var userId = $('body').data('user');        
+            var userGuid = $('body').data('guid');
         
-        if(userId != -1){ 
-            Native('registerForNotificationsCallback', 'uid:' + userGuid);
-        }
-        
-    }
-
-    document.addEventListener('turbolinks:load', function (e) { 
-        register();
-    });
-
-    register();
-})();
-
-
-
-
-
-
-/********************************************/
-/* Handle badge changes                     */
-/********************************************/
-weavyAppScripts.badge = (function(){    
-    wvy.realtime.on('badge.weavy', function(e, data){
-        Native('badgeCallback', data.conversations);
-    });
-
-    var check = function(){
-        
-        $.ajax('/a/conversations/unread?followed=true').then(function(response){
-            var count = response.data != null ? response.data.length : 0;
-                   
-            Native('badgeCallback', count);
-        });        
-    };
-    
-    check();
-
-    return {
-        check: check
-    };
-})();
-
-
-
-
-
-
-/********************************************/
-/* Handle theme                             */
-/********************************************/
-weavyAppScripts.theme = (function(){    
-    function set(){
-        var color = '#A15C08';
-                
-        $.ajax('/a/theme').then(function(response){
-            var colors = response.colors;
-            var color = response.theme_color;
-            if(!color && colors.length && colors.length > 7) {
-                color = colors[7];
+            if(userId != -1){ 
+                Native('registerForNotificationsCallback', 'uid:' + userGuid);
             }
+        
+        }
+
+        document.addEventListener('turbolinks:load', function (e) { 
+            register();
+        });
+
+        register();
+    })();
+
+
+
+    /********************************************/
+    /* Handle badge changes                     */
+    /********************************************/
+    weavyAppScripts.badge = (function(){    
+        wvy.connection.on('badge.weavy', function(e, data){
+            Native('badgeCallback', data);
+        });
+
+        var check = function(){
+        
+            $.ajax('/a/conversations/unread?followed=true').then(function(response){
+                var count = response.data != null ? response.data.length : 0;
                    
-            Native('themeCallback', color);
-        }).fail(function(e){
-            Native('themeCallback', color);
-        });
-    }
-
-    document.addEventListener('turbolinks:load', function (e) { 
-        set();
-    });
-
-    set();
-})();
-
-
-
-
-
-
-/********************************************/
-/* Sign-in and sign-out                     */
-/********************************************/
-weavyAppScripts.authentication = (function(){      
-
-    var signOut = function(){
-        $(document).on('click', 'a[href^=""/sign-out""]', function(){       
-    	    Native('signOutCallback', '');
-            return true;
-        });
-    };
+                Native('badgeCallback', {conversations: count, notifications: 0});
+            });        
+        };
     
-    signOut();
+        check();
 
-})();
+        return {
+            check: check
+        };
+    })();
 
 
+    /********************************************/
+    /* Handle theme                             */
+    /********************************************/
+    weavyAppScripts.theme = (function(){    
+        function set(){
+            var color = '#A15C08';
+                
+            $.ajax('/a/theme').then(function(response){
+                var colors = response.colors;
+                var color = response.theme_color;
+                if(!color && colors.length && colors.length > 7) {
+                    color = colors[7];
+                }
+                   
+                Native('themeCallback', color);
+            }).fail(function(e){
+                Native('themeCallback', color);
+            });
+        }
 
-/********************************************/
-/* Handle external links                    */
-/********************************************/
-weavyAppScripts.links = (function(){    
-    function handle(){
-       $(document).on('click', 'a[href^=http]', function (e) {
-	        var url = $(this).attr('href');
-            var target = $(this).attr('target');
-
-	        if(url.indexOf(window.location.origin) == -1 || target == '_blank'){
-			    e.preventDefault();	
-                Native('handleLinksCallback', url);
-	        }
+        document.addEventListener('turbolinks:load', function (e) { 
+            set();
         });
 
-        $(document).on('click', 'a[href^=ms-]', function (e) {
-	        e.preventDefault();		
-            var url = $(this).attr('href');
-            Native('handleLinksCallback', url);	
-        });
-    }
-
-    handle();
-})();
+        set();
+    })();
 
 
+    /********************************************/
+    /* Sign-in and sign-out                     */
+    /********************************************/
+    weavyAppScripts.authentication = (function(){      
+
+        var signOut = function(){
+            $(document).on('click', 'a[href^=""/sign-out""]', function(){       
+    	        Native('signOutCallback', '');
+                return true;
+            });
+        };
+    
+        signOut();
+
+    })();
 
 
+
+    /********************************************/
+    /* Handle external links                    */
+    /********************************************/
+    weavyAppScripts.links = (function(){    
+        function handle(){
+           $(document).on('click', 'a[href^=http]', function (e) {
+	            var url = $(this).attr('href');
+                var target = $(this).attr('target');
+
+	            if(url.indexOf(window.location.origin) == -1 || target == '_blank'){
+			        e.preventDefault();	
+                    Native('handleLinksCallback', url);
+	            }
+            });
+
+            $(document).on('click', 'a[href^=ms-]', function (e) {
+	            e.preventDefault();		
+                var url = $(this).attr('href');
+                Native('handleLinksCallback', url);	
+            });
+        }
+
+        handle();
+    })();
+
+
+
+}
 
 
 
